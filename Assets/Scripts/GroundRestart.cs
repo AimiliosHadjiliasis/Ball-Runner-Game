d7b4162f2474fa0f150caa1140e0694fba88ab73
Aimilios <aimilioshadjiliasis@gmail.com>
2020-06-23 19:11:43 +0300

    Import ground as trigger to restart the game

---
+++ Assets/Scripts/GroundRestart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GroundRestart : MonoBehaviour
+{
+    //Whenever player come in contact with this cube we restart the scene
+    void OnTriggerEnter()
+    {
+        SceneManager.LoadScene("MainScene");
+    }    
+}
