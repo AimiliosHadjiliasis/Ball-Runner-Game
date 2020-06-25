a1914b555dca5afba7ed041bd8b1cc107de7a5b7
Aimilios <aimilioshadjiliasis@gmail.com>
2020-06-25 15:55:43 +0300

    Add Rotation and up and down movement to the Coins

--- Assets/Scripts/RotateCoins.cs
+++ Assets/Scripts/RotateCoins.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class RotateCoins : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    Vector3 turnCoins;
+    float speed = 5.0f;
+    float height = 0.5f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        turnCoins = new Vector3(45,0,0);
+        //Rotate the coins:
+        transform.Rotate(turnCoins * Time.deltaTime);
+
     }
 }
